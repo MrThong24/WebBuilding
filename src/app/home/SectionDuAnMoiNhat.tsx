201d945f763778ad5fec7289dad23822963460d4
Bùi Chí Thông <@thong.buichi_tmp_pm3>
2024-12-30 14:27:13 +0700

    fix: fix bug

--- src/app/home/SectionDuAnMoiNhat.tsx
+++ src/app/home/SectionDuAnMoiNhat.tsx
@@ -9,7 +9,9 @@ import "swiper/css/pagination";
 import LayoutSection from "./layout/LayoutSection";
 import Image from "next/image";
 import DuAn1 from "../assest/image/DuAnMoiNhat/a1.jpg";
+import DuAn2 from "../assest/image/DuAnMoiNhat/a2.jpg";
 import Link from "next/link";
+
 export default function SectionDuAnMoiNhat() {
   return (
     <LayoutSection title="DỰ ÁN MỚI NHẤT" showBtnMore={true} slug="/projects">
@@ -59,6 +61,38 @@ export default function SectionDuAnMoiNhat() {
                     chuyên nghiệp, đảm bảo rằng mọi sân pickleball đều đạt tiêu
                     chuẩn cao nhất. Với thi công sân pickleball chú trọng vào
                     từng chi tiết, sẽ mang lại trải nghiệm cho khách hàng
+                  </p>
+                  <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-green-600/60">
+                    Xem chi tiết
+                  </button>
+                </div>
+              </div>
+            </Link>
+          </SwiperSlide>
+          <SwiperSlide className=" bg-slate-400">
+            <Link
+              href={`/projects/san-choi-the-thao/tim-hieu-chi-phi-thi-cong-san-pickleball`}
+            >
+              <div className="group relative cursor-pointer items-center justify-center overflow-hidden transition-shadow hover:shadow-xl hover:shadow-green-800/30">
+                <div className="max-h-[500px] w-auto">
+                  <Image
+                    alt="Công ty TNHH Đầu tư Phát triển Xây dựng Đại Tiến Phát"
+                    className="lg:h-[500px] sm:h-[460px] h-[400px] w-full object-cover transition-transform duration-500 group-hover:rotate-3 group-hover:scale-125"
+                    src={DuAn2}
+                  />
+                </div>
+                <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black ` group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70" />
+                <div className="absolute inset-12 flex translate-y-[60%] flex-col items-center justify-center px-5 text-center transition-all duration-500 group-hover:translate-y-0">
+                  <h1 className="text-[18px] font-bold text-white flex w-auto">
+                    Tìm hiểu chi phí thi công sân Pickleball
+                  </h1>
+                  <p className="mb-3 text-[14px] italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+                    Đại Tiến Phát hướng dẫn bạn cách tính toán chi phí thi công
+                    sân Pickleball, từ các yếu tố cơ bản như diện tích, vật liệu
+                    xây dựng, đến chi phí lắp đặt thiết bị và công lao động.
+                    Cùng với đó, bạn sẽ có cái nhìn tổng quan về ngân sách cần
+                    thiết để xây dựng một sân Pickleball chất lượng, phù hợp với
+                    nhu cầu sử dụng.
                   </p>
                   <button className="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-green-600/60">
                     Xem chi tiết
