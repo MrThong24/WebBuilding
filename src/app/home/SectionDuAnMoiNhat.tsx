d19dc218272ea9d0809b50109f9bd3763dd97af4
Bùi Chí Thông <@thong.buichi_tmp_pm3>
2025-02-05 15:35:21 +0700

    update code

--- src/app/home/SectionDuAnMoiNhat.tsx
+++ src/app/home/SectionDuAnMoiNhat.tsx
@@ -13,7 +13,11 @@ import Link from "next/link";
 
 export default function SectionDuAnMoiNhat() {
   return (
-    <LayoutSection title="DỰ ÁN MỚI NHẤT" showBtnMore={true} slug="/projects">
+    <LayoutSection
+      title="DỰ ÁN MỚI NHẤT"
+      showBtnMore={true}
+      slug="/projects/tat-ca"
+    >
       <div className="lg:mx-0 lg:max-w-none lg:grid-cols-3">
         <Swiper
           style={
