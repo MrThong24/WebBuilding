81deac0df61df0d1147e2cf06bbfc6dbe525a76a
Bùi Chí Thông <@thong.buichi_tmp_pm3>
2025-03-31 09:58:48 +0700

    add code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,12 @@ import Footer from "./layout/footer";
 import Header from "./layout/header";
 import Head from "next/head";
 import type { Metadata } from "next";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://daitienphatcons.com"),
   title: "CÔNG TY TNHH ĐẦU TƯ PHÁT TRIỂN XÂY DỰNG ĐẠI TIẾN PHÁT",
-  description: "CÔNG TY TNHH ĐẦU TƯ PHÁT TRIỂN XÂY DỰNG ĐẠI TIẾN PHÁT",
+  description: "Chuyên đầu tư và phát triển xây dựng tại Việt Nam.",
   icons: {
     icon: [
       {
@@ -18,9 +19,9 @@ export const metadata: Metadata = {
   },
   openGraph: {
     title: "CÔNG TY TNHH ĐẦU TƯ PHÁT TRIỂN XÂY DỰNG ĐẠI TIẾN PHÁT",
-    description: "CÔNG TY TNHH ĐẦU TƯ PHÁT TRIỂN XÂY DỰNG ĐẠI TIẾN PHÁT",
+    description: "Chuyên đầu tư và phát triển xây dựng tại Việt Nam.",
     images: "/images/favicon-light-custom.jpg",
-    url: `https://daitienphatcons.com`,
+    url: "https://daitienphatcons.com",
     type: "website",
   },
 };
@@ -33,8 +34,37 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
+        <title>Đại Tiến Phát - Xây Dựng & Đầu Tư</title>
+        <meta name="title" content="Đại Tiến Phát - Xây Dựng & Đầu Tư" />
+        <meta
+          name="description"
+          content="Chuyên đầu tư và phát triển xây dựng tại Việt Nam."
+        />
+        <meta
+          name="google-site-verification"
+          content="7AIi6IlmHq81QYPQ6YBBl4NayIcxJVnNMHQyLPRTD2g"
+        />
         <link rel="icon" href="/images/favicon-light.png" />
       </Head>
+      <Script
+        id="structured-data"
+        type="application/ld+json"
+        strategy="afterInteractive" // Chỉ chạy sau khi trang đã tải xong
+        dangerouslySetInnerHTML={{
+          __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "WebSite",
+            name: "Đại Tiến Phát",
+            url: "https://daitienphatcons.com",
+            potentialAction: {
+              "@type": "SearchAction",
+              target:
+                "https://daitienphatcons.com/search?q={search_term_string}",
+              "query-input": "required name=search_term_string",
+            },
+          }),
+        }}
+      />
       <body>
         <div className="bg-white">
           <Header />
